81f346e748a2ad3f4009e41c40d4071e92540c35
hadisamerjomaa <hadi.jomaa@vwfs.io>
2023-03-11 17:04:39 +0100

    carried over transformers

---
+++ modules/networks.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+
+
+class BaseAttention(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.layernorm = tf.keras.layers.LayerNormalization()
+        self.add = tf.keras.layers.Add()
+
+
+class CrossAttention(BaseAttention):
+    def call(self, x, context):
+        attn_output, attn_scores = self.mha(
+            query=x,
+            key=context,
+            value=context,
+            return_attention_scores=True)
+
+        # Cache the attention scores for plotting later.
+        self.last_attn_scores = attn_scores
+
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+
+        return x
+
+
+class GlobalSelfAttention(BaseAttention):
+    def call(self, x):
+        attn_output = self.mha(
+            query=x,
+            value=x,
+            key=x)
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+        return x
+
+
+class CausalSelfAttention(BaseAttention):
+    def call(self, x):
+        attn_output = self.mha(
+            query=x,
+            value=x,
+            key=x,
+            use_causal_mask=True)
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+        return x
+
+
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, d_model, dff, dropout_rate=0.1):
+        super().__init__()
+        self.seq = tf.keras.Sequential([
+            tf.keras.layers.Dense(dff, activation='relu'),
+            tf.keras.layers.Dense(d_model),
+            tf.keras.layers.Dropout(dropout_rate)
+        ])
+        self.add = tf.keras.layers.Add()
+        self.layer_norm = tf.keras.layers.LayerNormalization()
+
+    def call(self, x):
+        x = self.add([x, self.seq(x)])
+        x = self.layer_norm(x)
+        return x
+
+
+class VariationalMLP(tf.keras.layers.Layer):
+    def __init__(self, d_model, dff, dropout_rate=0.1):
+        super(VariationalMLP, self).__init__()
+
+        self.penultimate_layer = tf.keras.Sequential([
+            tf.keras.layers.Dense((d_model + dff) // 2, activation='relu'),
+            tf.keras.layers.Dropout(dropout_rate)
+        ])
+
+        self.mean_layer = tf.keras.layers.Dense(dff)
+        self.std_layer = tf.keras.layers.Dense(dff)
+
+    def forward(self, x):
+        hidden = self.penultimate_layer(x)
+
+        # Then apply further linear layers to output latent mu and log sigma
+        mu = self.mean_layer(hidden)
+        log_sigma = self.std_layer(hidden)
+
+        # Bound the variance
+        sigma = 0.1 + 0.9 * tf.nn.softplus(log_sigma)
+
+        return tf.concat([mu,sigma],axis=-1)
---
+++ modules/transformers.py
@@ -0,0 +1,172 @@
+import tensorflow as tf
+import networks
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, *, d_model, num_heads, dff, dropout_rate=0.1):
+        super().__init__()
+
+        self.self_attention = networks.GlobalSelfAttention(
+            num_heads=num_heads,
+            key_dim=d_model,
+            dropout=dropout_rate)
+
+        self.ffn = networks.FeedForward(d_model, dff)
+
+    def call(self, x):
+        x = self.self_attention(x)
+        x = self.ffn(x)
+        return x
+
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, *, num_layers, d_model, num_heads,
+                 dff, dropout_rate=0.1):
+        super().__init__()
+
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.ffn = tf.keras.Sequential([
+            tf.keras.layers.Dense(d_model, activation='relu'),
+            tf.keras.layers.Dropout(dropout_rate)
+        ])
+
+        self.enc_layers = [
+            EncoderLayer(d_model=d_model,
+                         num_heads=num_heads,
+                         dff=dff,
+                         dropout_rate=dropout_rate)
+            for _ in range(num_layers)]
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+    def call(self, x):
+        # `x` is token-IDs shape: (batch, seq_len)
+        x = self.ffn(x)  # Shape `(batch_size, seq_len, d_model)`.
+
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x)
+
+        return x  # Shape `(batch_size, seq_len, d_model)`.
+
+
+class DecoderLayer(tf.keras.layers.Layer):
+    def __init__(self,
+                 *,
+                 d_model,
+                 num_heads,
+                 dff,
+                 dropout_rate=0.1):
+        super(DecoderLayer, self).__init__()
+
+        self.last_attn_scores = None
+        self.self_attention = networks.GlobalSelfAttention(
+            num_heads=num_heads,
+            key_dim=d_model,
+            dropout=dropout_rate)
+
+        self.cross_attention = networks.CrossAttention(
+            num_heads=num_heads,
+            key_dim=d_model,
+            dropout=dropout_rate)
+
+        self.ffn = networks.FeedForward(d_model, dff)
+
+    def call(self, x, context):
+        x = self.self_attention(x=x)
+        x = self.cross_attention(x=x, context=context)
+
+        # Cache the last attention scores for plotting later
+        self.last_attn_scores = self.cross_attention.last_attn_scores
+
+        x = self.ffn(x)  # Shape `(batch_size, seq_len, d_model)`.
+        return x
+
+
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self, *, num_layers, d_model, num_heads, dff,
+                 dropout_rate=0.1):
+        super(Decoder, self).__init__()
+
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.dec_layers = [
+            DecoderLayer(d_model=d_model, num_heads=num_heads,
+                         dff=dff, dropout_rate=dropout_rate)
+            for _ in range(num_layers)]
+
+        self.last_attn_scores = None
+
+    def call(self, x, context):
+        # `x` is token-IDs shape (batch, target_seq_len)
+
+        x = self.dropout(x)
+
+        for i in range(self.num_layers):
+            x = self.dec_layers[i](x, context)
+
+        self.last_attn_scores = self.dec_layers[-1].last_attn_scores
+
+        # The shape of x is (batch_size, target_seq_len, d_model).
+        return x
+
+
+class Transformer(tf.keras.Model):
+    def __init__(self, *, num_layers, d_model, num_heads, dff,
+                 num_latent, dropout_rate=0.1):
+        super().__init__()
+        self.ffn = tf.keras.Sequential([
+            tf.keras.layers.Dense(d_model, activation='relu'),
+            tf.keras.layers.Dropout(dropout_rate)
+        ])
+        self.encoder = Encoder(num_layers=num_layers, d_model=d_model,
+                               num_heads=num_heads, dff=dff,
+                               dropout_rate=dropout_rate)
+
+        self.decoder = Decoder(num_layers=num_layers, d_model=d_model,
+                               num_heads=num_heads, dff=dff,
+                               dropout_rate=dropout_rate)
+
+        self.final_layer = networks.VariationalMLP(d_model=d_model, dff=num_latent, dropout_rate=dropout_rate)
+
+    def call(self, inputs):
+        # To use a Keras model with `.fit` you must pass all your inputs in the
+        # first argument.
+        context, x = inputs
+        context = self.ffn(context)
+
+        x = self.ffn(x)
+        context = self.encoder(context)  # (batch_size, context_len, d_model)
+
+        x = self.decoder(x, context)  # (batch_size, target_len, d_model)
+
+        # Final linear layer output.
+        logits = self.final_layer(x)  # (batch_size, target_len, target_vocab_size)
+
+        try:
+            # Drop the keras mask, so it doesn't scale the losses/metrics.
+            # b/250038731
+            del logits._keras_mask
+        except AttributeError:
+            pass
+
+        # Return the final output and the attention weights.
+        return logits
+
+
+if __name__ == "__main__":
+    import numpy as np
+    num_layers = 2
+    d_model = 64
+    num_heads = 8
+    dff = 1024
+    num_latent = 32
+    dropout_rate = 0.1
+
+    transformer = Transformer(num_layers=num_layers, num_heads=num_heads, dropout_rate=dropout_rate,
+                              dff=dff, d_model=d_model, num_latent=num_latent)
+    context = tf.convert_to_tensor(np.random.rand(64, 10, 5))
+    x = tf.convert_to_tensor(np.random.rand(64, 3, 5))
+    out = transformer((context, x))
