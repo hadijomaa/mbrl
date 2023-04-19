0afd76514d7e80d4aeec99ebba2419594986004b
hadi-jomaa <hadi.jomaa@vwfs.io>
2023-04-19 23:46:17 +0200

    bug fix

--- collect_test_results.py
+++ collect_test_results.py
@@ -11,18 +11,21 @@ if __name__ == "__main__":
     args.apply_lookahead = bool(args.apply_lookahead)
     args.load_pretrained = bool(args.load_pretrained)
 
-    log_path = os.path.join("./results", "pre-trained" if args.load_pretrained  else "random-initialization",
+    log_path = os.path.join("./results", "pre-trained" if args.load_pretrained else "random-initialization",
                             args.search_space, f"horizon-{args.horizon}",
                             f"trajectories-{args.num_random_trajectories}", f"particles-{args.num_particles}",
-                            f"{'LookAhead' if args.apply_lookahead  else 'MPC'}", args.inference_optimizer,
-                            f"{args.inference_learning_rate}", f"mpc-{args.mpc_seed}")
-
+                            f"{'LookAhead' if args.apply_lookahead else 'MPC'}", args.inference_optimizer,
+                            f"lr-{args.inference_learning_rate}", f"mpc-{args.mpc_seed}")
+    found = 0
+    not_found = 0
     for dataset_id in os.listdir(log_path):
         # construct full file path
         dataset_folder = os.path.join(log_path, dataset_id)
-        for subfolders in os.listdir(dataset_folder):
-            results_folder = os.path.join(dataset_folder, subfolders)
-            try:
-                pd.read_csv(os.path.join(results_folder, "results.csv"))
-            except Exception as e:
-                print(dataset_id)
+        try:
+            pd.read_csv(os.path.join(dataset_folder, "results.csv"));
+            found += 1
+        except Exception as e:
+            not_found += 1
+    print(
+        f"found {found}; not found {not_found} , {args.num_particles}, {args.apply_lookahead}, {args.load_pretrained}, {args.num_random_trajectories}")
+
