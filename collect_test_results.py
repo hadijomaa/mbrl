064f22fee2fb446f4f34c5265b151e54a5c3a999
hadi-jomaa <hadi.jomaa@vwfs.io>
2023-04-26 18:51:37 +0200

    bugs fixed

--- collect_test_results.py
+++ collect_test_results.py
@@ -4,13 +4,15 @@ import subprocess
 import pandas as pd
 from helpers import parsers
 
-template_script = "cat /work/ws/nemo/fr_hj1023-LookAhead-0/mbrl/scripts/sbatch/{search_space}/joint/{subfolder1}/cs_seed-45/test/horizon-{horizon}/trajectories-{trajectories}/particles-{particles}/{subfolder2}/mpc-{mpc}/{dataset_id}.sh"
+template_script = "{command} /work/ws/nemo/fr_hj1023-LookAhead-0/mbrl/scripts/sbatch/{search_space}/joint/{subfolder1}/cs_seed-45/test/horizon-{horizon}/trajectories-{trajectories}/particles-{particles}/{subfolder2}/mpc-{mpc}/{dataset_id}.sh"
 
 if __name__ == "__main__":
     parser = parsers.get_pets_parser()
     parser = parsers.get_hp_parser(parser)
+    parser.add_argument('--rerun', type=int, default=1, choices=[0, 1], help='re-run failed experiments')
     args = parser.parse_args()
     args.apply_lookahead = bool(args.apply_lookahead)
+    args.rerun = bool(args.rerun)
     args.load_pretrained = bool(args.load_pretrained)
 
     log_path = os.path.join("./results", "pre-trained" if args.load_pretrained else "random-initialization",
@@ -30,8 +32,9 @@ if __name__ == "__main__":
             not_found += 1
             subfolder1 = "pre-trained" if args.load_pretrained else "random-initialization"
             subfolder2 = "LookAhead" if args.apply_lookahead else "MPC"
-            print("Rerunning missing")
-            rerun_command = template_script.format(search_space=args.search_space,
+
+            rerun_command = template_script.format(command="cat" if not args.rerun else "msub",
+                                                   search_space=args.search_space,
                                                    subfolder1=subfolder1,
                                                    subfolder2=subfolder2,
                                                    horizon=args.horizon,
@@ -40,10 +43,7 @@ if __name__ == "__main__":
                                                    mpc=args.mpc_seed,
                                                    dataset_id=dataset_id,
                                                    )
-            print(rerun_command)
-            process = subprocess.Popen(rerun_command.split(), stdout=subprocess.PIPE)
-            output, error = process.communicate()
-            print(error)
-
+            print(f"Rerunning missing using command: {rerun_command}")
+            subprocess.run(rerun_command.split(" "))
     print(
-        f"found {found}; not found {not_found} , {args.num_particles}, {args.horizon}, {args.apply_lookahead}, {args.load_pretrained}, {args.num_random_trajectories}")
+	f"found {found}; not found {not_found} , {args.num_particles}, {args.horizon}, {args.apply_lookahead}, {args.load_pretrained}, {args.num_random_trajectories}")
