658d007aa730655474c68faa22241f13dec7167f
hadi-jomaa <hadi.jomaa@vwfs.io>
2023-04-29 17:13:16 +0200

    rerun files with no result

--- validate_test_results.py
+++ validate_test_results.py
@@ -23,29 +23,47 @@ if __name__ == "__main__":
                             f"lr-{args.inference_learning_rate}", f"mpc-{args.mpc_seed}")
     found = 0
     not_found = 0
-    for dataset_id in os.listdir(log_path):
-        # construct full file path
-        dataset_folder = os.path.join(log_path, dataset_id)
-        try:
-            pd.read_csv(os.path.join(dataset_folder, "results.csv"))
-            found += 1
-        except Exception as e:
-            not_found += 1
-            subfolder1 = "pre-trained" if args.load_pretrained else "random-initialization"
-            subfolder2 = "LookAhead" if args.apply_lookahead else "MPC"
+    try:
+        for dataset_id in os.listdir(log_path):
+            # construct full file path
+            dataset_folder = os.path.join(log_path, dataset_id)
+            try:
+                pd.read_csv(os.path.join(dataset_folder, "results.csv"))
+                found += 1
+            except Exception as e:
+                not_found += 1
+                subfolder1 = "pre-trained" if args.load_pretrained else "random-initialization"
+                subfolder2 = "LookAhead" if args.apply_lookahead else "MPC"
 
-            rerun_command = template_script.format(command="cat" if not args.rerun else "msub",
-                                                   search_space=args.search_space,
-                                                   subfolder1=subfolder1,
-                                                   subfolder2=subfolder2,
-                                                   horizon=args.horizon,
-                                                   trajectories=args.num_random_trajectories,
-                                                   particles=args.num_particles,
-                                                   mpc=args.mpc_seed,
-                                                   dataset_id=dataset_id,
-                                                   )
-            if args.rerun:
-                print(f"Rerunning missing using command: {rerun_command}")
-                subprocess.run(rerun_command.split(" "))
-    print(
-	f"found {found}; not found {not_found} , {args.num_particles}, {args.horizon}, {args.apply_lookahead}, {args.load_pretrained}, {args.num_random_trajectories}")
+                rerun_command = template_script.format(command="cat" if not args.rerun else "msub",
+                                                       search_space=args.search_space,
+                                                       subfolder1=subfolder1,
+                                                       subfolder2=subfolder2,
+                                                       horizon=args.horizon,
+                                                       trajectories=args.num_random_trajectories,
+                                                       particles=args.num_particles,
+                                                       mpc=args.mpc_seed,
+                                                       dataset_id=dataset_id,
+                                                       )
+                if args.rerun:
+                    print(f"Rerunning missing using command: {rerun_command}")
+                    subprocess.run(rerun_command.split(" "))
+        print(
+        f"found {found}; not found {not_found} , {args.num_particles}, {args.horizon}, {args.apply_lookahead}, {args.load_pretrained}, {args.num_random_trajectories}")
+    except Exception as ex:
+        subfolder1 = "pre-trained" if args.load_pretrained else "random-initialization"
+        subfolder2 = "LookAhead" if args.apply_lookahead else "MPC"
+
+        rerun_command = "{command} for particles in {particles}; do for horizon in {horizon}; do for trajectories in {trajectories};  do for file in scripts/sbatch/{search_space}/joint/{subfolder1}/cs_seed-45/test/horizon-$horizon/trajectories-$trajectories/particles-$particles/{subfolder2}/mpc-{mpc}/*; do msub $file;done;done;done;done;done;done;"
+        rerun_command = template_script.format(command="echo" if not args.rerun else "",
+                                               search_space=args.search_space,
+                                               subfolder1=subfolder1,
+                                               subfolder2=subfolder2,
+                                               horizon=args.horizon,
+                                               trajectories=args.num_random_trajectories,
+                                               particles=args.num_particles,
+                                               mpc=args.mpc_seed
+                                               )
+        if args.rerun:
+            print(f"Rerunning missing using command: {rerun_command}")
+            subprocess.run(rerun_command.split(" "))
